31570fa3dafd47b76e98d5c0eccb6365b85c2d60
pan.lu@mtime.com <pan.lu@mtime.com>
2017-09-22 18:34:29 +0800

    node联系

--- 第6部分-nodejs/02-express/04-example/app.js
+++ 第6部分-nodejs/02-express/04-example/app.js
@@ -28,6 +28,43 @@ app.use(app.router);
 // 目录为demo/public/images，访问网址则显示为http://localhost:3000/images
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 配置路由
+// 所谓“路由”，就是指为不同的访问路径，指定不同的处理方法。
+/*
+app.get('/', function(req, res) {
+    var body = 'Hello World';
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', body.length);
+    res.end(body);
+});
+*/
+
+// 指定特定路径
+// 假定用户访问/api路径，希望返回一个JSON字符串
+/*app.get('/api', function(req, res) {
+    response.send({name: "张三", age: 40});
+});*/
+
+// 可以把app.get的回调函数，封装成模块。现在routes目录下面建立一个api.js文件。
+/*
+var api = require('./routes/api');
+app.get('/api', api.index);
+*/
+
+// 静态网页模板
+app.get('/', function (req, res) {
+    res.sendfile('./views/index.html');
+});
+
+app.get('/about', function (req, res) {
+    res.sendfile('./views/about.html');
+});
+
+app.get('/article', function (req, res) {
+    res.sendfile('./views/article.html');
+});
+
+app.listen(3000);
+
 // 调用实例方法listen，让其监听事先设定的端口(3000)
 app.listen(app.get("port"));
-
---
+++ 第6部分-nodejs/02-express/04-example/routes/api.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mtime on 2017/9/22.
+ */
+
+exports.index = function(req, res) {
+    res.json(200, {name:"张三",age:40});
+};
