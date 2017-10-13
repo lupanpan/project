6d6814ca3f437114aa6b45d7e63723e1462170a1
pan.lu@mtime.com <pan.lu@mtime.com>
2017-10-13 20:32:27 +0800

    node blog

--- 第6部分-nodejs/06-myblog/routes/users.js
+++ 第6部分-nodejs/06-myblog/routes/users.js
@@ -5,7 +5,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name', function (req, res) {
-    res.send('hello,' + req.params.name);
+    // 使用express的方式渲染模板视图
+    res.render('users', {
+        name: req.params.name
+    })
 });
 
 module.exports = router;
+
+/*
+通过调用 res.render 函数渲染 ejs 模板，res.render 第一个参数是模板的名字，这里是 users 则会匹配 views/users.ejs，
+第二个参数是传给模板的数据，这里传入 name，则在 ejs 模板中可使用 name。res.render 的作用就是将模板和数据结合生成 html，
+同时设置响应头中的 Content-Type: text/html，告诉浏览器我返回的是 html，不是纯文本，要按 html 展示。现在我们访问 localhost:3000/users/haha*/
