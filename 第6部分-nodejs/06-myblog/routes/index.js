c55b7e479f7e10f7b2f68c85c5f9725ef372f09d
pan.lu@mtime.com <pan.lu@mtime.com>
2017-12-15 19:26:58 +0800

    开发node

--- 第6部分-nodejs/06-myblog/routes/index.js
+++ 第6部分-nodejs/06-myblog/routes/index.js
@@ -2,11 +2,18 @@
  * Created by Mtime on 2017/10/16.
  */
 module.exports = function (app) {
+  // 根目录，跳转查看文章
   app.get('/', function (req, res) {
     res.redirect('/posts')
   })
+  // 注册
   app.use('/signup', require('./signup'))
+  // 登录
   app.use('/signin', require('./signin'))
-  app.use('./signout', require('./signout'))
-  app.use('./posts', require('./posts'))
+  // 登出
+  app.use('/signout', require('./signout'))
+  // 查看文章
+  app.use('/posts', require('./posts'))
+  // 留言
+  app.use('/comments', require('./'))
 }
