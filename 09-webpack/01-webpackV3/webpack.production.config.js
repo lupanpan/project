93b6c22529260087ef38c989f3f1247c37dd4cdf
pan.lu@mtime.com <pan.lu@mtime.com>
2018-01-05 15:20:39 +0800

    webpack 插件使用

--- 09-webpack/01-webpackV3/webpack.production.config.js
+++ 09-webpack/01-webpackV3/webpack.production.config.js
@@ -4,6 +4,8 @@
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     devtool: "null", //注意修改了这里，这能大大压缩我们的打包代码
@@ -11,7 +13,7 @@ module.exports = {
     entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
     output: {
         path: __dirname + "/build", //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
+        filename: "bundle-[hash].js" //打包后输出文件的文件名
     },
 
     devServer: {
@@ -57,6 +59,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html" //new一个这个插件的实例，并传入相关的参数
         }),
-        new webpack.HotModuleReplacementPlugin() //热加载插件
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css"),
+        new webpack.HotModuleReplacementPlugin(), //热加载插件
+        new CleanWebpackPlugin('build/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ]
 }
