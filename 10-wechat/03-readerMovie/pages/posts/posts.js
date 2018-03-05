1d1b077df8b1ca8ee33edfd49154f236b837f4d1
lupanpan <767416500@qq.com>
2018-03-05 23:28:28 +0800

    小程序开发

---
+++ 10-wechat/03-readerMovie/pages/posts/posts.js
@@ -0,0 +1,13 @@
+var postsData = require('../../data/posts-data.js')
+
+Page({
+  data: {
+    // 小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
+    // 而这个动作A的执行，是在onLoad函数执行之后发生的
+  },
+  onLoad: function () {
+    this.setData({
+      postList:postsData.postList
+    })
+  }
+})
