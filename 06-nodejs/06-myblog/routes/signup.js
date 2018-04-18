d55d2b6adf9d4adb59e1ddfa1969a7a51d112ff3
pan.lu@mtime.com <pan.lu@mtime.com>
2018-04-18 18:59:04 +0800

    node bug 修改

--- 06-nodejs/06-myblog/routes/signup.js
+++ 06-nodejs/06-myblog/routes/signup.js
@@ -1,8 +1,13 @@
 /**
  * Created by lupan on 2017/12/16.
  */
+const fs = require('fs')
+const path = require('path')
+const sha1 = require('sha1')
 const express = require('express')
 const router = express.Router()
+
+const UserModel = require('../models/users')
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 // GET /signup 注册页
