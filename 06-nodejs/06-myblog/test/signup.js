ad28f2f80ffced33dd8dc4acb6b3ba42b449231b
pan.lu@mtime.com <pan.lu@mtime.com>
2018-05-18 19:08:51 +0800

    开发测试部分

--- 06-nodejs/06-myblog/test/signup.js
+++ 06-nodejs/06-myblog/test/signup.js
@@ -3,3 +3,38 @@ const assert = require('assert')
 const request = require('supertest')
 const app = require('../index')
 const User = require('../lib/mongo').User
+
+const testName1 = 'testName1'
+const testName2 = 'nswbmw'
+
+describe('signup', function () {
+  describe('POST /signup', function () {
+    // persist cookie when redirect
+    const agent = request.agent(app)
+    beforeEach(function (done) {
+      // 创建一个用户
+      User.create({
+        name: testName1,
+        password: '123456',
+        avatar: '',
+        gender: 'x',
+        bio: ''
+      })
+        .exec()
+        .then(function () {
+          done()
+        })
+        .catch(done)
+    })
+
+    afterEach(function (done) {
+      // 删除测试用户
+      User.deleteMany({ name: { $in: [testName1, testName2] } })
+        .exec()
+        .then(function () {
+          done()
+        })
+        .catch(done)
+    })
+  })
+})
