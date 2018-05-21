c47fa1c76df5f820dd3e2db69e002a3476bd42e2
pan.lu@mtime.com <pan.lu@mtime.com>
2018-05-21 18:21:23 +0800

    开发node测试功能

--- 06-nodejs/06-myblog/test/signup.js
+++ 06-nodejs/06-myblog/test/signup.js
@@ -36,5 +36,70 @@ describe('signup', function () {
         })
         .catch(done)
     })
+
+    after(function (done) {
+      process.exit()
+    })
+
+    // 用户名错误的情况
+    it('wrong name', function (done) {
+      agent
+        .post('/signup')
+        .type('form')
+        .attach('avatar', path.join(__dirname, 'avatar.png'))
+        .field({ name: '' })
+        .redirects()
+        .end(function (err, res) {
+          if (err) return done(err)
+          assert(res.text.match(/名字请限制再 1-10 个字符/))
+          done()
+        })
+    })
+
+    // 性别错误的情况
+    it('wrong gender', function (done) {
+      agent
+        .post('/signup')
+        .type('form')
+        .attach('avatar', path.join(__dirname, 'avatar.png'))
+        .field({ name: testName2, gender: 'a' })
+        .redirects()
+        .end(function (err, res) {
+          if (err) return done(err)
+          assert(res.text.match(/性别只能是 m、f 或 x/))
+          done()
+        })
+    })
+
+    // 其余的参数测试自行补充
+    // 用户名被占用的情况
+    it('duplicate name', function (done) {
+      agent
+        .post('/signup')
+        .type('form')
+        .attach('avatar', path.join(__dirname, 'avatar.png'))
+        .field({ name: testName1, gender: 'm', bio: 'gender', password: '123456', repassword: '123456' })
+        .redirects()
+        .end(function (err, res) {
+          if (err) return done(err)
+          assert(res.text.match(/用户名已被占用/))
+          done()
+        })
+    })
+
+    // 注册成功的情况
+    it('success', function (done) {
+      agent
+        .post('/signup')
+        .type('form')
+        .attach('avatar', path.join(__dirname, 'avatar.png'))
+        .field({ name: testName2, gender: 'm', bio: 'gender', password: '123456', repassword: '123456' })
+        .redirects()
+        .end(function (err, res) {
+          if (err) return done(err)
+          assert(res.text.match(/注册成功/))
+          done()
+        })
+    })
   })
 })
