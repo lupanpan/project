5ec5d4addbb759551407f5ade046db1aed593e26
pan.lu@mtime.com <pan.lu@mtime.com>
2018-06-21 19:22:04 +0800

    开发知乎日报小程序的详情页

--- 10-wechat/06-zhihu/pages/detail/detail.js
+++ 10-wechat/06-zhihu/pages/detail/detail.js
@@ -44,6 +44,89 @@ Page({
   },
 
   /**
+   * 收藏与取消收藏
+   */
+  collectOrNot: function ()  {
+    var pageData = wx.getStorageSync('pageData') || [];
+
+    // 判断当前的收藏状态
+    if (this.data.isCollect) {
+      // 遍历本地存储缓存中收藏的文章
+      for (let i = 0; i < pageData.length; i++) {
+        // 根据id获取该文章
+        if (pageData[i].id == this.data.id) {
+          // 根据index从缓存中移除该文章
+          pageData.splice(i, 1);
+          // 设置收藏状态
+          this.setData({ isCollect: false })
+        }
+      }
+    }
+    else {
+      var images = new Array(this.data.news.image);
+      var item = { id: this.data.id, title: this.data.news.title, images: images };
+      // 追加到数组开头
+      pageData.unshift(item);
+      this.setData({ isCollect: true })
+    }
+
+    // 设置缓存
+    try{
+      wx.setStorageSync("pageData", pageData);
+    } catch (e) { }
+    console.log(pageData);
+  },
+
+  /**
+   * 跳转到评论页面
+   */
+  toCommentPage: function (e) {
+    var storyId = e.currentTarget.dataset.id;
+    // 长评数目
+    var longCommentCount = this.data.extraInfo ? this.data.extraInfo.long_comments : 0;
+    // 短评数目
+    var shortCommentCount = this.data.extraInfo ? this.data.extraInfo.short_comments : 0;
+
+    // 跳转到评论页面，并传递评论数目信息
+    wx.navigateTo({
+      url: '../comment/comment?lcount=' + longCommentCount + '&scount=' + shortCommentCount + '&id=' + storyId
+    });
+  },
+
+  /**
+   * 图片预览
+   */
+  previewImgEvent: function (e) {
+    var src = e.currentTarget.dataset.src;
+    if (src && src.length > 0) {
+      wx.previewImage({
+        urls: [src]
+      })
+    }
+  },
+
+  /**
+   * 重新加载数据
+   */
+  reloadEvent: function () {
+    this.loadData();
+  },
+
+  /**
+   * 显示分享模态框
+   */
+  showModalEvent: function () {
+    this.setData({ modalHidden: false });
+  },
+
+  /**
+   * 隐藏分享模态框
+   */
+  hideModalEvent: function () {
+    this.setData({ modalHidden: true });
+  },
+
+  /**
    * 加载页面相关数据
    */
   loadData: function () {
@@ -72,6 +155,8 @@ Page({
 
     requests.getStoryExtraInfo(this.data.id, (data) => {
       console.log('extra', data);
+      // 设置点赞数量大于999显示999+
+      data.dispose_popularity = (data.popularity > 999) ? "999+" : data.popularity;
       _this.setData({ extraInfo: data });
     })
   }
