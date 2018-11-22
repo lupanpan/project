db140b3b559c25979768d5b853b71a983911b08c
pan.lu@mtime.com <pan.lu@mtime.com>
2018-11-22 18:58:31 +0800

    写react

--- 08-react/06-createReact/contacts/src/ImageInput.js
+++ 08-react/06-createReact/contacts/src/ImageInput.js
@@ -11,28 +11,40 @@ const readFileAsDataURL = (file) => {
 
         reader.readAsDataURL(file)
     })
-}
 
-const resizeImage = (imageURL, canvas, maxHeight) =>
-    new Promise(resolve => {
-        const image = new Image()
+    const resizeImage = (imageURL, canvas, maxHeight) =>
+        new Promise(resolve => {
+            const image = new Image()
 
-        image.onload = () => {
-            const context = canvas.getContext('2d')
+            image.onload = () => {
+                const context = canvas.getContext('2d')
 
-            if (image.height > maxHeight) {
-                image.width *= maxHeight / image.height
-                image.height = maxHeight
+                if (image.height > maxHeight) {
+                    image.width *= maxHeight / image.height
+                    image.height = maxHeight
+                }
+
+                context.clearRect(0, 0, canvas.width, canvas.height)
+                canvas.width = image.width
+                canvas.height = image.height
+
+                context.drawImage(image, 0, 0, image.width, iamge.height)
+
+                resolve(canvas.toDataURL('image/jpeg'))
             }
 
-            context.clearRect(0, 0, canvas.width, canvas.height)
-            canvas.width = image.width
-            canvas.height = image.height
+            image.src = imageURL
+        })
 
-            context.drawImage(image, 0, 0, image.width, iamge.height)
+    /**
+     * A custom <input> that dynamically reads and resizes image files before
+     * submitting them to the server as data URLs. Also, shows a preview of the image.
+     * 一个自定义<input>，在此之前动态读取和调整图像文件的大小以数据url的形式提交给服务器。同时，显示图像的预览。
+     */
+    class ImageInput extends React.Component {
 
-            resolve(canvas.toDataURL('image/jpeg'))
-        }
+    }
+}
 
-        image.src = imageURL
-    })
+
+
