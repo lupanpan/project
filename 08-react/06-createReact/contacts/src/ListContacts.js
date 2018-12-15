81c7845750ee9ff5af72c2aaf302d1c811cc6e3a
pan.lu@mtime.com <pan.lu@mtime.com>
2018-12-15 19:39:22 +0800

    修改react

--- 08-react/06-createReact/contacts/src/ListContacts.js
+++ 08-react/06-createReact/contacts/src/ListContacts.js
@@ -1,26 +1,30 @@
 import React, {Component} from 'react'
 
-class ListContacts extends Component {
-    render() {
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div className="contact-avatar" style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}/>
-                        <div className="contact-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+function ListContacts(props) {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map((contacts) => (
+                <li key={contacts.id} className='contact-list-item'>
+                    <div className="contact-avatar" style={{
+                        backgroundImage: `url(${contacts.avatarURL})`
+                    }}></div>
+                    <div className="contact-details">
+                        <p>{contacts.name}</p>
+                        <p>{contacts.email}</p>
+                    </div>
+                    <button className="contact-remove">
+                        remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
 }
+
+// class ListContacts extends Component {
+//     render() {
+//
+//     }
+// }
 
 export default ListContacts
